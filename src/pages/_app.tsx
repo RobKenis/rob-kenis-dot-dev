79c42894779ab2cacaf681bf28cd4be6535cc31c
Rob Kenis <robke@softwareag.com>
2023-02-13 15:58:13 +0100

    Setup NextUI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,29 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import {createTheme, NextUIProvider} from "@nextui-org/react"
+import {ThemeProvider as NextThemesProvider} from 'next-themes';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const lightTheme = createTheme({
+    type: 'light',
+})
+
+const darkTheme = createTheme({
+    type: 'dark',
+})
+
+export default function App({Component, pageProps}: AppProps) {
+    return (
+        <NextThemesProvider
+            defaultTheme="system"
+            attribute="class"
+            value={{
+                light: lightTheme.className,
+                dark: darkTheme.className
+            }}
+        >
+            <NextUIProvider>
+                <Component {...pageProps} />
+            </NextUIProvider>
+        </NextThemesProvider>
+    );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
-import {Button} from '@/components/Button'
+import {useTheme as useNextTheme} from "next-themes";
+import {Switch, useTheme} from "@nextui-org/react";
 
 export default function Home() {
+    const { setTheme } = useNextTheme();
+    const { isDark, type } = useTheme();
     return (
         <>
-            <Button label={"This is a button"}/>
+            <div>
+                The current theme is: {type}
+                <Switch
+                    checked={isDark}
+                    onChange={(e) => setTheme(e.target.checked ? 'dark' : 'light')}
+                />
+            </div>
         </>
     )
 }
