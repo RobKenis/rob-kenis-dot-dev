2cc19a157df74a3759e37fad85f9e8c789581df7
Rob Kenis <robke@softwareag.com>
2024-01-24 08:25:44 +0100

    Remove everything
    Let's be honest, I'm not building a proper frontend
    any time soon.

--- src/pages/404.tsx
+++
@@ -1,7 +0,0 @@
-import {Link, useTheme} from "@nextui-org/react";
-import ErrorPage from 'next/error'
-
-export default function FourOhFour() {
-    const {isDark, type} = useTheme();
-    return <ErrorPage withDarkMode={isDark} statusCode="404" />
-}
